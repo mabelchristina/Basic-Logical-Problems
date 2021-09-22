cdeb52cd268c7bdceb2da00e6f1cf05a4d8d68c0
Mabel <maybelchristina@gmail.com>
2021-09-22 14:54:31 +0530

    [Mabel] Add: UC1 added

---
+++ BasicLogicalPrograms/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicLogicalPrograms
+{
+    class Fibonacci
+    {
+        //public void CalculateFibonacci(int length)
+        //{
+        //    int val1 = 0, val2 = 1, val3;
+        //    for (int i = 2; i < length; i++)
+        //    {
+        //        val3 = val1 + val2;
+        //        Console.Write(val3 + "  ");
+        //        val1 = val2;
+        //        val2 = val3;
+        //    }
+        //}
+        public void CalculateFibRecursion(int val1, int val2, int counter, int number)
+        {
+            Console.WriteLine(val1);
+            if (counter < number)
+            {
+                CalculateFibRecursion(val2, val1 + val2, counter + 1, number);
+            }
+        }
+    }
+}
---
+++ BasicLogicalPrograms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BasicLogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fibonacci fibonacci = new Fibonacci();
+            //fibonacci.CalculateFibonacci(Convert.ToInt32(Console.ReadLine()));
+            int number = Convert.ToInt32(Console.ReadLine());
+            fibonacci.CalculateFibRecurrsion(0,1,1,number);
+        }
+    }
+}

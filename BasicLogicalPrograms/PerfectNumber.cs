b54f61b7efd46ce3f95671317834ee585d66e249
Mabel <maybelchristina@gmail.com>
2021-09-22 15:36:06 +0530

    [Mabel] Add: UC2 added

---
+++ BasicLogicalPrograms/PerfectNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicLogicalPrograms
+{
+    class PerfectNumber
+    {
+        public bool IsPerfect(int number)
+        {
+            int sum = 0;
+            for (int value = 1; value < number; value++)
+            {
+                if (number % value == 0)
+                    sum = sum + value;
+            }
+            if (sum == number)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- BasicLogicalPrograms/Program.cs
+++ BasicLogicalPrograms/Program.cs
@@ -6,10 +6,28 @@ namespace BasicLogicalPrograms
     {
         static void Main(string[] args)
         {
-            Fibonacci fibonacci = new Fibonacci();
-            //fibonacci.CalculateFibonacci(Convert.ToInt32(Console.ReadLine()));
-            int number = Convert.ToInt32(Console.ReadLine());
-            fibonacci.CalculateFibRecurrsion(0,1,1,number);
+            Console.WriteLine("1.Fibonacci Series\n 2.Perfect Number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Fibonacci fibonacci = new Fibonacci();
+                    //fibonacci.CalculateFibonacci(Convert.ToInt32(Console.ReadLine()));
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    fibonacci.CalculateFibRecursion(0, 1, 1, number);
+                    break;
+                case 2:
+                    bool value = false;
+                    PerfectNumber perfect = new PerfectNumber();
+                    Console.WriteLine("Enter a number \n");
+                    value=perfect.IsPerfect(Convert.ToInt32(Console.ReadLine()));
+                    if (value)
+                        Console.WriteLine("Given number is perfect number");
+                    else
+                        Console.WriteLine("Given number is not perfect number");
+                    break;
+            }
+
         }
     }
 }
